d274d3ac6b5bbc7b3f7b41da53a9084131176821
Fran Sanjuán <francesc.sanjuan@marfeel.com>
2024-05-17 17:53:54 +0200

    Change function

--- postgresql-patroni/entrypoint.sh
+++ postgresql-patroni/entrypoint.sh
@@ -16,7 +16,7 @@ export PATRONI_SUPERUSER_PASSWORD="$SU_PASS"
 export PATRONI_appuser_PASSWORD="$POSTGRES_APP_ROLE_PASS"
 export PATRONI_appuser_OPTIONS="${PATRONI_admin_OPTIONS:-createdb, createrole}"
 
-function exit_container_SIGTERM {
+exit_container_SIGTERM() {
   pg_ctl -D $POSTGRESQL_CONTAINER_DATA_DIR/postgresql stop
   exit 0
 }
@@ -24,4 +24,4 @@ function exit_container_SIGTERM {
 #--- trap the SIGTERM signal
 trap 'exit_container_SIGTERM' SIGTERM
 
-$HOME/.local/bin/patroni /etc/patroni.yml
+exec $HOME/.local/bin/patroni /etc/patroni.yml
